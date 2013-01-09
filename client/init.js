0e32d8f28c0c5f0ed33531d74961fb40986019dc
Jonathan Myers <hello@jnthnmyrs.com>
2013-01-09 15:13:32 -0700

    Removed some unnecessary commented-out code chunks

--- client/init.js
+++ client/init.js
@@ -4,83 +4,13 @@ $(document).ready( function () {
     $(document).on('drop', function (e) {
         e.preventDefault();
     });
-
-    // $("body").on('click', function(){
-    //     Session.set("selected_thumbnail", undefined);
-    //     console.log('unset');
-    // });
-
-    //$('img.lazy').lazyload();
-
 });
-
-
-//--------------------------------------------------
-//  Session variables
-//--------------------------------------------------
-
-// ID of currently selected list
-Session.set('list_id', null);
-
-// Name of currently selected tag for filtering
-Session.set('tag_filter', null);
-
-// When adding tag to a picture, ID of the picture
-Session.set('editing_addtag', null);
-
-// When editing a list name, ID of the list
-Session.set('editing_listname', null);
-
-// When editing todo text, ID of the todo
-Session.set('editing_itemname', null);
-
-
-//--------------------------------------------------
-// Helpers for in-place editing
-//--------------------------------------------------
-// Returns an event map that handles the "escape" and "return" keys and
-// "blur" events on a text input (given by selector) and interprets them
-// as "ok" or "cancel".
-
-var okCancelEvents = function (selector, callbacks) {
-  var ok = callbacks.ok || function () {};
-  var cancel = callbacks.cancel || function () {};
-
-  var events = {};
-  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-    function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
-
-      } else if (evt.type === "keyup" && evt.which === 13 ||
-                 evt.type === "focusout") {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
-      }
-    };
-  return events;
-};
-
-var activateInput = function (input) {
-  input.focus();
-  input.select();
-};
-
 
 
 
 //--------------------------------------------------
 //  Sidebar
 //--------------------------------------------------
-
-Template.sidebar.rendered = function () { 
-
-};
 
 Template.sidebar.events({
 
@@ -131,17 +61,16 @@ Template.sidebar.hiddenSearchUpload = function () {
     };
 };
 
-// if (Session.get("selected_thumbnail")) {
-//     $('#searchUpload').hide('fast');
-// } else {
-//     $('#searchUpload').show();
-// };
 
-
-Template.sidebar.about = function () {
+// I've placed these two variables (phraseArray and randomNumber)
+// outside of the Template.sidebar.about function so that it
+// won't update every time the sidebar is rendered.  I suspect 
+// there is a nicer way to go about this.  - Jonathan
 // This guy is here to create "random" little things that show up in the upper-left corner right under "Corkboard"
-    var phraseArray = ["Share your work.", "Get feedback.", "Give tips.", "Pass it on.", "Work fast.", "Converse.", "Capitalize.", "Achieve.", "Sharpen.", "Proof read."]
-    return phraseArray[0]; // (Math.floor(Math.random()*10)
+var phraseArray = ["", "Share your work.", "Get feedback.", "Give tips.", "Pass it on.", "Work fast.", "Converse.", "Capitalize.", "Achieve.", "Sharpen.", "Proof read."]
+var randomNumber=Math.floor(Math.random()*11)
+Template.sidebar.about = function () {
+    return phraseArray[randomNumber];
 };
 
 Template.sidebar.currentThumb = function () {
@@ -161,27 +90,12 @@ Template.sidebar.selectedTitle = function () {
     };
 };
 
-//--------------------------------------------------
-//  Trying a hacky thing to unset the "selected_thumbnail" variable
-//--------------------------------------------------
-// Template.allTheContent.events({
-//     'click' : function() {
-//         return Session.set("selected_thumbnail", undefined);
-//     }
-
-// });
-
 
 //--------------------------------------------------
 //  Gallery
 //--------------------------------------------------
 Template.gallery.thumbnails = function() {
     return Pictures.find({},{sort: {timestamp: -1}});
-    // return Pictures.find({}, {
-    //   sort: {
-    //     timestamp: 1,
-    //   }
-    // });
 };
     
 
@@ -197,6 +111,10 @@ Template.thumbnail.selected = function() {
     } else {
       return "";
     }
+};
+
+Template.thumbnail.postAuthor = function() {
+    return this.userId;
 };
 
 
@@ -232,11 +150,6 @@ Template.thumbnail.events({
   }
 
 });
-
-// $(":not(.selected)").click(function(){
-//         return Session.set("selected_thumbnail", undefined);
-//     });
-
 
 
 //--------------------------------------------------
@@ -293,57 +206,6 @@ Template.commentList.events = ({
 
 });
 
-
-
-//--------------------------------------------------
-//  Tracking selected list in URL
-//--------------------------------------------------
-// var TodosRouter = Backbone.Router.extend({
-//   routes: {
-//     ":list_id": "main"
-//   },
-//   main: function (list_id) {
-//     Session.set("list_id", list_id);
-//     Session.set("tag_filter", null);
-//   },
-//   setList: function (list_id) {
-//     this.navigate(list_id, true);
-//   }
-// });
-
-// Router = new TodosRouter;
-
-// Meteor.startup(function () {
-//   Backbone.history.start({pushState: true});
-// });
-
-
-// var Router = Backbone.Router.extend({
-//   routes: {
-//     "":                 "main", //this will be http://your_domain/
-//     "help":             "help"  // http://your_domain/help
-//   },
-
-//   main: function() {
-//     // Your homepage code
-//     // for example: Session.set('currentPage', 'homePage');
-//     Session.set('currentPage', 'homePage');
-//   },
-
-//   help: function() {
-//     // Help page
-//   }
-// });
-// var app = new Router;
-// Meteor.startup(function () {
-//   Backbone.history.start({pushState: true});
-// });
-
-
-
-//--------------------------------------------------
-//  Other stuff
-//--------------------------------------------------
 
 
 
